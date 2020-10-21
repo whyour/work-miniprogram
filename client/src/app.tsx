6fd3a6388f595339c5374b6b04efca595dec6267
whyour <imwhyour@gmail.com>
2020-10-21 13:17:39 +0800

    添加版本更新检查

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -21,7 +21,30 @@ class App extends Component {
       });
   }
 
-  componentDidShow() {}
+  componentDidShow() {
+    const updateManager = Taro.getUpdateManager()
+    updateManager.onCheckForUpdate((res) => {
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(() => {
+          Taro.showModal({
+            title:'更新提示',
+            content:'检测到新版本, 是否下载新版本并重启小程序?',
+            success: (res) => {
+              if (res.confirm) {
+                updateManager.applyUpdate()
+              }
+            },
+          })
+        })
+        updateManager.onUpdateFailed(() => {
+          Taro.showModal({
+            title:'已经有新版本了哟~',
+            content:'新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
+          })
+        })
+      }
+    })
+  }
 
   componentDidHide() {}
 
